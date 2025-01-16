a0e67c5771568ac6ea46c5ccb0f52c9322d2d1dc
johnnystouffer <stouff17@msu.edu>
2025-01-15 21:22:26 -0500

    Binary tree inverting and level order update

--- Trees/102_BinaryTreeLevelOrder.py
+++ Trees/102_BinaryTreeLevelOrder.py
@@ -42,5 +42,50 @@ class Solution:
 
         # return all the nodes that are given
         return listOfNodes
+    
+
+"""
+Attempt 2:
+
+Pretty similar to the first attempt thankfully
+Other than that I am glad to report that all is
+good for me right now
+"""
+from typing import Optional, List
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q = deque()
+        q.append((root, 0))
+
+
+        currLevel = -1
+        finalList = []
+        while q:
+            currNode, level = q.popleft()
+            if level > currLevel:
+                finalList.append([])
+                currLevel = level
+
+            finalList[-1].append(currNode.val)
+
+            if currNode.left:
+                q.append((currNode.left, level+1))
+            if currNode.right:
+                q.append((currNode.right, level+1))
+
+        return finalList
+
+            
+
+            
             
 
--- Trees/226_InvertABinaryTree.py
+++ Trees/226_InvertABinaryTree.py
@@ -25,3 +25,4 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+    
