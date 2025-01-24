6920a63fcc04c71df265741f3a63ccaaa7bbe815
johnnystouffer <stouff17@msu.edu>
2025-01-24 14:15:59 -0500

    UPDATED Course Schedule so I can do it now woohoo

--- Topological/207_CourseSchedule.py
+++ Topological/207_CourseSchedule.py
@@ -41,3 +41,44 @@ class Solution:
                     q.append(neighbor)
 
         return len(finalOrder) == numCourses
+
+
+"""
+RETRY:
+
+After not being on leetcode for a millenium it feels like, I am back and I have
+attempted this problem again... And I shit you not it looks the EXACT SAME
+
+The only difference I want to point out is that my variable names this time around
+look significantly better than before, thank you.
+"""
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+
+        # make a dictionary for the number of prereqs left for this course
+        numPrereq = {i: 0 for i in range(numCourses)}
+        # make a dictionary for all the classes this one is a prereq for
+        nextCourses = {i: [] for i in range(numCourses)}
+
+        for post, pre in prerequisites:
+            # add a prereq and add it to the list for pre
+            numPrereq[post] += 1
+            nextCourses[pre].append(post)
+
+        q = deque([i for i in numPrereq if numPrereq[i] == 0])
+        finalOrder = set()
+        
+        while q:
+            value = q.popleft()
+            finalOrder.add(value)
+
+            for nextClass in nextCourses[value]:
+                numPrereq[nextClass] -= 1
+                if numPrereq[nextClass] == 0:
+                    q.append(nextClass)
+        
+        return len(finalOrder) == numCourses
