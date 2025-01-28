25827238c44d1043f87c32a634f4b9e0364ac59c
johnnystouffer <stouff17@msu.edu>
2025-01-27 20:56:31 -0500

    Changing to C++

---
+++ Arrays/1_TwoSum.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+/**
+ * 1. Two Sum
+ * 
+ * I have decided to start using C++ as my leetcode language!
+ * Why would I do such a thing? Well the truth is although python
+ * is more intuitive, C++ actually makes the harder problems easier
+ * thanks to the STL.
+ * 
+ * Also because I want to be a cracked dev, and cracked devs do not
+ * use python.
+ * 
+ * Overall we all know Two Sum, I just wanted to do this to start going
+ * in C++. Wish me luck!
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // make local variables for answer and the hashmap to store
+        vector<int> answer;
+        unordered_map<int, int> hashMap;
+
+        // loop through each item and its index
+        for (int i = 0; i < nums.size(); i++) {
+
+            // see what the complement is
+            int newSum = target - nums[i];
+
+            // if the complement is in the hashtable we found the pair
+            if (hashMap.find(newSum) != hashMap.end()) {
+                answer.push_back(hashMap[newSum]);
+                answer.push_back(i);
+                return answer;
+            }
+            else {
+                hashMap[nums[i]] = i;
+            }   
+        }
+        return answer;
+    }
+};
