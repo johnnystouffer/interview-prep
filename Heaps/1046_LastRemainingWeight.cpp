b6c5c82696f3580f847bbc3f60127d5d757d4721
johnnystouffer <stouff17@msu.edu>
2025-02-06 16:56:02 -0500

    Last remaining stone

---
+++ Heaps/1046_LastRemainingWeight.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/**
+ * 1046. Last Reaining Stone
+ * 
+ * Use a priority queue, 
+ */
+
+ class Solution {
+    public:
+        int lastStoneWeight(vector<int>& stones) {
+            priority_queue<int> pq(stones.begin(), stones.end());
+    
+            while (pq.size() > 1) {
+                int left = pq.top();
+                pq.pop();
+                int right = pq.top();
+                pq.pop();
+    
+                if (left != right) { pq.push(left - right); } 
+            }
+    
+            return pq.empty() ? 0 : pq.top();
+        }
+    };
