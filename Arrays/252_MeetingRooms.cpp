8d57142b5160a5e4f7a985e11ba5b82405dd460d
johnnystouffer <stouff17@msu.edu>
2025-02-10 21:53:12 -0500

    We have the libraries folder now woohoo

--- Arrays/252_MeetingRooms.cpp
+++ Arrays/252_MeetingRooms.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include "../libraries.h"
 
 using namespace std;
 
---
+++ libraries.h
@@ -0,0 +1,8 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <vector>
+#include <queue>
+#include <cmath>
+
+#endif
