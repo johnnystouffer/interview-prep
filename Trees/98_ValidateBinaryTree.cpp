56d01be2be1553e03e1fb632134eac1db324906c
johnnystouffer <stouff17@msu.edu>
2025-02-19 23:38:03 -0500

    Adding updated Binary Search Tree

--- Trees/98_ValidateBinaryTree.cpp
+++ Trees/98_ValidateBinaryTree.cpp
@@ -41,3 +41,36 @@ class Solution {
             return false;
         }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ /**
+  * New and improved Version...
+  * 
+  * This version just stores that last number and then checks if it needs to be returned
+  */
+class Solution {
+    public:
+        std::vector<int> prev;
+        bool isValidBST(TreeNode* root) {
+            if (!root) return true;
+    
+            if(!isValidBST(root->left)) return false;
+    
+            if (!prev.empty() && root->val <= prev[prev.size() - 1]) return false;
+    
+            prev.push_back(root->val);
+    
+            return isValidBST(root->right);
+        }
+    };
