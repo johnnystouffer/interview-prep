9d80aea4e51a9ea9d9c46344cb63ae8647a4568f
john-stouffer <john@apsdatatechnologies.com>
2025-03-11 22:47:06 -0400

    redo binary search

--- Arrays/704_BinarySearch.cpp
+++ Arrays/704_BinarySearch.cpp
@@ -32,3 +32,24 @@ public:
         return -1;
     }
 };
+
+/**
+ * This is my splution after becoming more comfortable with C++
+ */
+
+class Solution {
+    public:
+        int search(vector<int>& nums, int target) {
+            int l = 0, r = nums.size() - 1;
+    
+            while(l <= r) {
+                int mid = (l + r) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] > target) { r = mid - 1; }
+                else { l = mid + 1; }
+            }
+            return -1;
+        }
+    };
