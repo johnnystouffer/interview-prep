f96d5498391033d1edad2ad23fbe23d021d52db2
john-stouffer <john@apsdatatechnologies.com>
2025-03-11 23:03:39 -0400

    adding tabulation for climbing stairs

--- Dynamic Programming/70_ClimbingStairs.cpp
+++ Dynamic Programming/70_ClimbingStairs.cpp
@@ -24,3 +24,27 @@ public:
         return mapping[n];
     }
 };
+
+/**
+ * I did it both ways, memoization and tabulation
+ * memoization is more intuitive for me, however tabulization is better
+ * so I want to get better at it.
+ */
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        vector<int> dp(n);
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        return dp[n-1];
+    }
+};
