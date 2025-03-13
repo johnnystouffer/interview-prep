db2ec7ef634d38584cc537aa54042f7cebf047a1
john-stouffer <john@apsdatatechnologies.com>
2025-03-13 18:34:02 -0400

    A better generate parentheses

--- Stack/22_GenerateParentheses.cpp
+++ Stack/22_GenerateParentheses.cpp
@@ -39,3 +39,30 @@ class Solution {
  * The only thing I would reccomend for next time is do back track, that way you do not need to allocate more memory
  * to the stack for every little thing that happens. So yeah, that & is very powerful though.
  */
+
+class Solution {
+ public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        string curr = "(";
+        findPermutations(v, curr, 1, 0, n);    
+        return v;    
+    }
+
+    void findPermutations(vector<string>& v, string& curr, int currOpen, int currClosed, int n) {        
+        if (curr.size() == 2*n && currOpen == n && currClosed == n) {
+            v.push_back(curr);
+            return;
+        }
+        if (currOpen < n) {
+            curr.push_back('(');
+            findPermutations(v, curr, currOpen + 1, currClosed, n);
+            curr.pop_back();
+        }
+        if (currClosed < currOpen) {
+            curr.push_back(')');
+            findPermutations(v, curr, currOpen, currClosed + 1, n);
+            curr.pop_back();
+        }
+    }
+};
