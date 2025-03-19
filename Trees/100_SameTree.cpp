1468567f0e28f5c36b96e6fe17c3ba5e6bd0055b
john-stouffer <stouff17@msu.edu>
2025-03-19 11:47:19 -0400

    Redoing the same tree easy problem

--- Trees/100_SameTree.cpp
+++ Trees/100_SameTree.cpp
@@ -28,3 +28,21 @@ public:
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
+
+/**
+ * Attempt 2:a
+ * 
+ * It looks the EXACT same besides I know c++ a little better now so it is a little easier
+ * woohoo I guess
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) { return true; }
+        if (!p && q || p && !q || p->val != q->val) {
+            return false;
+        }
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
